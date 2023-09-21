a8d64f4b19602105742d274d980e201a993f6492
YuyaItabashi3594 <asuholover@gmail.com>
2023-09-21 18:04:25 +0900

    画像を移動できるように

--- components/StartPositionCanvas.vue
+++ components/StartPositionCanvas.vue
@@ -1,45 +1,52 @@
 <script setup lang="ts">
 import { VugelMouseEvent } from 'vugel';
 
-const dragTarget: Ref<(Node | null)[]> = ref([])
-let startEvent: VugelMouseEvent | null = null;
-let startX = 0;
-let startY = 0;
-
-const start = (index,e: VugelMouseEvent) => {
-  console.log(index)
-  if (dragTarget.value[index]) {
-    startEvent = e;
-
-    // As Vugel doesn't expose the properties, we refer directly to the element.
-    startX = dragTarget.value[index].el.x as number;
-    startY = dragTarget.value[index].el.y as number;
-  }
-};
-
-const move = (index,e: VugelMouseEvent,) => {
-  console.log(index)
-  if (startEvent) {
-    const deltaX = e.canvasOffsetX - startEvent.canvasOffsetX;
-    const deltaY = e.canvasOffsetY - startEvent.canvasOffsetY;
-    dragTarget.value[index]!.x = startX + deltaX;
-    dragTarget.value[index]!.y = startY + deltaY;
-
-    // Do not scroll on mobile.
-    e.originalEvent.preventDefault();
-  }
-};
-
-const end = (index,e: VugelMouseEvent) => {
-  startEvent = null;
-};
-
 const props = defineProps({
   deck: {
     type: Array,
     required: true
   }
 })
+
+const dragTarget: Ref<(Node | null)[]> = ref([])
+let startEvent: VugelMouseEvent | null = null;
+let startX = [];
+let startY = [];
+
+const start = (e: VugelMouseEvent) => {
+  props.deck.forEach((card, index) => {
+    console.log(card.img)
+    if (card.img === e.target.tex.source.lookupId && dragTarget.value) {
+      console.log('if is true')
+      startEvent = e;
+      // As Vugel doesn't expose the properties, we refer directly to the element.
+      startX[index] = dragTarget.value[index].el.x as number;
+      startY[index] = dragTarget.value[index].el.y as number;
+    }
+  })
+
+};
+
+const move = (e: VugelMouseEvent,) => {
+  props.deck.forEach((card, index) => {
+    if (card.img === e.target.tex.source.lookupId && startEvent) {
+      console.log('if is true move')
+      const deltaX = e.canvasOffsetX - startEvent.canvasOffsetX;
+      const deltaY = e.canvasOffsetY - startEvent.canvasOffsetY;
+      dragTarget.value[index]!.x = startX[index] + deltaX;
+      dragTarget.value[index]!.y = startY[index] + deltaY;
+
+      // Do not scroll on mobile.
+      e.originalEvent.preventDefault();
+    }
+  })
+};
+
+const end = (e: VugelMouseEvent) => {
+  startEvent = null;
+};
+
+
 
 
 
@@ -54,6 +61,7 @@ start, move, end
 <template compiler="vugel">
   <rectangle :h="10" :x="0" :y="200" :w="1000" color="rgb(0, 255, 0)" />
   <container :flex="true">
-    <picture v-for="(card, index) in deck" :ref="dragTarget[index]" :src="card.img" :w="120" :h="192" :pivot="0.0" @mousedown="start(index)" @mousemove="move(index)" @mouseup="end(index)" @mouseleave="end(index)" />
+    <picture v-for="(card, index) in deck" :ref="el => dragTarget[index] = el" :src="card.img" :rotation="0.0" :w="120" :h="192"
+      :pivot="0.0" @mousedown="start" @mousemove="move" @mouseup="end" @mouseleave="end" />
   </container>
 </template>
